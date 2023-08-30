1e7fadee3f2738efb7506d0c8405c430b6414abd
Cédric Bontems <67456035+cbontems@users.noreply.github.com>
2023-08-30 18:42:52 +0200

    build: bump all deps and asrto to v3
    BREAKING CHANGE:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 import solidJs from "@astrojs/solid-js";
 
 const toc = (str) => {
@@ -11,12 +10,5 @@ const toc = (str) => {
 // https://astro.build/config
 export default defineConfig({
 	site: "https://cedricbontems.fr/",
-	integrations: [
-		robotsTxt(),
-		sitemap(),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
-		solidJs(),
-	],
+	integrations: [robotsTxt(), sitemap(), solidJs()],
 });
--- src/helpers/authors.ts
+++ src/helpers/authors.ts
@@ -1,22 +1,9 @@
-import type { ImageMetadata } from "@astrojs/image/dist/vite-plugin-astro-image.js";
-import { CollectionEntry } from "astro:content";
-
-const allImages = import.meta.glob<{ default: ImageMetadata }>(
-	"/src/content/authors/_images/*.{png,jpg,jpeg,webp}",
-);
+import type { CollectionEntry } from "astro:content";
 
 export const resolveImage = async (entry: CollectionEntry<"authors">) => {
 	if (!entry.data.image) {
 		return undefined;
 	}
 
-	if (!(entry.data.image in allImages)) {
-		throw new Error(
-			`[authors] Image for "${entry.data.title}" not found! Provided: "${entry.data.image}", is there a typo?`,
-		);
-	}
-
-	const { default: image } = await allImages[entry.data.image]();
-
-	return image;
+	return entry.data.image;
 };
